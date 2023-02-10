6a700af028e5fad9312c570730dd0f72e8c42e81
enlaitc <thiago021299@hotmail.com>
2023-02-10 20:17:34 -0300

    handle exceptions entityNotFound e HttpMessageNotReadable

--- app/src/main/kotlin/com/project/quizapi/app/dataprovider/oracle/QuizPostgresProvider.kt
+++ app/src/main/kotlin/com/project/quizapi/app/dataprovider/oracle/QuizPostgresProvider.kt
@@ -4,11 +4,14 @@ import com.project.quizapi.app.dataprovider.oracle.model.toEntity
 import com.project.quizapi.app.dataprovider.oracle.repository.QuizRepository
 import com.project.quizapi.domain.dataprovider.QuizDataProvider
 import com.project.quizapi.domain.entity.QuizEntity
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
 class QuizPostgresProvider(private val quizRepository: QuizRepository) : QuizDataProvider {
     override fun findQuizById(id: Long): QuizEntity {
-        return quizRepository.findById(id).get().toEntity()
+        val quiz = quizRepository.findById(id)
+        if(quiz.isEmpty) throw EntityNotFoundException("Quiz not found")
+        return quiz.get().toEntity()
     }
 }
